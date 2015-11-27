c60c9c85401b13f3ab45a71b76f5a65527363fa0
Shujia Huang <huangshujia9@gmail.com>
2015-11-27 17:11:08 +0800

    cool! success connecting to the mongodb

---
+++ app/models.py
@@ -0,0 +1,11 @@
+import datetime
+
+from app import db
+
+class Todo(db.Document):
+    """
+    """
+    content = db.StringField(required = True, max_length = 20)
+    time = db.DateTimeField(default = datetime.datetime.now())
+    status = db.IntField(default = 0)
+
