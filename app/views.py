6cfe09ca4ddd257ef709a917962374373a852fcd
Shujia Huang <huangshujia9@gmail.com>
2015-11-27 18:01:35 +0800

    add function: update the list on the index page

--- app/views.py
+++ app/views.py
@@ -1,4 +1,9 @@
+"""
+Copyright (c) Shujia Huang
+Date: 2015-11-27
+"""
 from flask import render_template
+from flask import request
 
 from app import app
 from models import Todo
@@ -7,3 +12,11 @@ from models import Todo
 def index():
     todos = Todo.objects.all()
     return render_template('index.html', todos = todos)
+
+@app.route('/add', methods = ['POST',])
+def add():
+    content = request.form.get("content")
+    todo = Todo(content = content)
+    todo.save()
+    todos = Todo.objects.all()
+    return render_template("index.html", todos = todos)
