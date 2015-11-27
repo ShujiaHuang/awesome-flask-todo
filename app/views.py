3e3a39d1352045d03139a178d62cf620585a9aa6
Shujia Huang <huangshujia9@gmail.com>
2015-11-27 17:37:48 +0800

    show the message to index.html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
-from app import app
 from flask import render_template
+
+from app import app
+from models import Todo
 
 @app.route('/')
 def index():
-    return render_template('index.html', text = 'Hello world')
+    todos = Todo.objects.all()
+    return render_template('index.html', todos = todos)
--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 # -- * -- coding; utf-8 -- * --
 from flask.ext.script import Manager, Server
+
 from app import app
 from app.models import Todo
 
